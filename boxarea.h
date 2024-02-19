cb9f4573be3ffd5b2f7dbd4bd0b90a3a463efdc9
dineshnaidubudi <158827289+dineshnaidubudi@users.noreply.github.com>
2024-02-19 15:05:05 +0530

    Create boxarea.h

---
+++ boxarea.h
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+
+ void boxArea(float length , float width ,float height){
+    cout<<"The area of the box : "<<(2*(length*width)*(width*height)*(height*length))<<endl;
+ }
+
