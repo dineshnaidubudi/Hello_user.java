54ceaca3873c98236ff6fc43c9764cbc8813a5e9
dineshnaidubudi <158827289+dineshnaidubudi@users.noreply.github.com>
2024-03-05 19:20:06 +0530

    Update simpleinhertiance.java

--- simpleinhertiance.java
+++ simpleinhertiance.java
@@ -1,57 +1,31 @@
-class Vehicle {
-    protected String brand;
-    protected int year;
-
-    public Vehicle(String brand, int year) {
-        this.brand = brand;
-        this.year = year;
+class dog {
+    public
+    void name() {
+        System.out.print("Name of the dog is bruno \n ");
     }
 
-    public void displayInfo() {
-        System.out.println("Brand: " + brand);
-        System.out.println("Year: " + year);
+    void owner() {
+        System.out.print("My owner is pandu\n");
     }
 }
 
-class Car extends Vehicle {
-    protected int numDoors; // Change private to protected
-
-    public Car(String brand, int year, int numDoors) {
-        super(brand, year);
-        this.numDoors = numDoors;
+class puppy extends dog {
+    public
+    void play() {
+        System.out.print("he is hyper active \n");
     }
 
-    @Override
-    public void displayInfo() {
-        super.displayInfo();
-        System.out.println("Number of doors: " + numDoors);
+    void skills() {
+        System.out.print("He can easily sense the objects\n");
     }
 }
 
-class Bike extends Vehicle {
-    private String type;
-
-    public Bike(String brand, int year, String type) {
-        super(brand, year);
-        this.type = type;
-    }
-
-    @Override
-    public void displayInfo() {
-        super.displayInfo();
-        System.out.println("Type: " + type);
-    }
-}
-
-public class Main {
+class SimpleInheritance {
     public static void main(String[] args) {
-        Car myCar = new Car("Toyota", 2022, 4);
-        Bike myBike = new Bike("Honda", 2020, "Mountain");
-
-        System.out.println("Car Information:");
-        myCar.displayInfo();
-
-        System.out.println("\nBike Information:");
-        myBike.displayInfo();
+        puppy baby = new puppy();
+        baby.name();
+        baby.owner();
+        baby.play();
+        baby.skills();
     }
 }
