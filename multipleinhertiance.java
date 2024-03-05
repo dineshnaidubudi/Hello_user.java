cbf057cad72db7038e9a1bc5ede5ee1df2e5bc2d
dineshnaidubudi <158827289+dineshnaidubudi@users.noreply.github.com>
2024-03-05 19:26:13 +0530

    Update multipleinhertiance.java

--- multipleinhertiance.java
+++ multipleinhertiance.java
@@ -3,7 +3,7 @@ class ClassA
     public
         void aClass()
         {
-            System.out.print("Iam a ClassA"+"\n");
+            System.out.print("Iam a parent ClassA"+"\n");
         }
 }
 
@@ -12,7 +12,7 @@ class ClassB extends ClassA
     public
         void bClass()
         {
-            System.out.print("Iam a ClassB"+"\n");
+            System.out.print("Iam a child ClassB"+"\n");
         }
 }
 
@@ -21,39 +21,20 @@ class ClassC extends ClassA
     public
         void cClass()
         {
-            System.out.print("Iam a ClassC"+"\n");
+            System.out.print("Iam a child  ClassC"+"\n");
         }
 }
 
-class ClassD extends ClassB
-{
-    public
-        void dClass()
-        {
-            System.out.print("Iam a ClassD"+"\n");
-        }
-}
-class InheritanceTypes
+class DaimondProblem 
 {
     public static void main(String[] args)
     {
-        //Simple Inheritance
-        System.out.print("Simple Inheritance"+"\n");
+        System.out.print("Diamond Problem"+"\n");
         ClassB class1=new ClassB();
-        class1.aClass();
-        class1.bClass();
-        //Hierarchical Inheritance
-        System.out.print("Hierarchical Inheritance"+"\n");
         ClassC class2=new ClassC();
         class1.aClass();
         class1.bClass();
         class2.aClass();
         class2.cClass();
-        //Multi-Level Inheritance
-        System.out.print("Multi-Level Inheritance"+"\n");
-        ClassD class3=new ClassD();
-        class3.aClass();
-        class3.bClass();
-        class3.dClass();
-    }
+           }
 }
