2f76ffc5d929e9dec5e29f7c3fc2dce90fe49260
dineshnaidubudi <158827289+dineshnaidubudi@users.noreply.github.com>
2024-04-25 09:25:47 +0530

    Create filehandling.java

---
+++ filehandling.java
@@ -0,0 +1,18 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.*;
+class ex{
+    public static void main(String[] args) throws IOException {
+        FileInputStream x;
+        FileOutputStream y;
+        try{
+            x=new FileInputStream("file.txt");
+            y=new FileOutputStream("copy.txt");
+            int ch;
+            while((ch=x.read())!=-1){
+                y.write(ch);
+            }
+        }
+        catch(IOException e){e.printStackTrace();}
+    }
+}
